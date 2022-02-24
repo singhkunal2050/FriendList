0e20ddb9eb5a0379442dcbe582e1fe4cea0d1bd7
singhkunal2050 <singhkunal2050@gmail.com>
2022-02-24 16:23:33 +0530

    added edge case for name

--- src/components/AddFriend.jsx
+++ src/components/AddFriend.jsx
@@ -6,26 +6,30 @@ function AddFriend() {
 
   const addFriend = (e) => {
     e.preventDefault();
-    setFriends([
-      ...friends,
-      { id: friends.length + 1, name: friend, star: false },
-    ]);
-    console.log(friends);
-    setFriend("");
+    if (friend.name !== "") {
+      setFriends([
+        ...friends,
+        { id: friends.length + 1, name: friend, star: false },
+      ]);
+      console.log(friends);
+      setFriend("");
+    }else{
+      alert("Please enter a name");
+    }
   };
 
   return (
-      <section>
-        <form onSubmit={addFriend}>
-          <input
-            type="text"
-            placeholder="Add Friend"
-            value={friend}
-            onChange={(e) => setFriend(e.target.value)}
-            className="add-friend-input"
-          />
-        </form>
-      </section>
+    <section>
+      <form onSubmit={addFriend}>
+        <input
+          type="text"
+          placeholder="Add Friend"
+          value={friend}
+          onChange={(e) => setFriend(e.target.value)}
+          className="add-friend-input"
+        />
+      </form>
+    </section>
   );
 }
 
